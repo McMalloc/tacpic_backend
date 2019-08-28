7015504b81be7f107944091b9f1c8569d85ccd50
mcmalloc <kuckieg0>
2019-08-28 18:28:04 +0200

    migrations

--- db/migrations/020_create_payments.rb
+++ db/migrations/020_create_payments.rb
@@ -2,11 +2,12 @@ class CreatePayments < Sequel::Migration
   def up
     create_table :payments do
       primary_key :id
-      foreign_key :invoice_id
-      foreign_key :order_id
 
-      Integer :status, null: false, default: 1
-      String :comment, text: true
+      Integer :user_id # not not null, maybe the payment was made from an unknown account
+      Integer :invoice_id # not not null, maybe there was a misguided transaction
+      BigDecimal :base_price, null: false, size: [10, 4]
+      String :service # what information is being transmitted when a payment arrives?
+      String :payed_by
       DateTime :created_at
     end
   end
