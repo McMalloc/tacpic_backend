73f55cd11bdc3a0312eee73f40637454cff8fd9a
mcmalloc <kuckieg0>
2019-09-23 18:23:29 +0200

    * added example test case

--- tests/graphic_tests.rb
+++ tests/graphic_tests.rb
@@ -13,3 +13,34 @@ require_relative "./test_helper"
 #     puts $test_db[:graphics][0]
 #   end
 # end
+
+describe "routes" do
+  it "..." do
+    data = {
+        user_id: 0,
+        title: "a valid title for a graphic",
+        is_request: false
+    }
+    post 'graphics', data.to_json
+
+    assert_equal 202, last_response.status
+    puts $test_db[:graphics][0]
+  end
+
+  it "should get the correct graphic" do
+    random_id = (rand 9).to_i
+    get "graphic/#{random_id}"
+
+    assert_equal 200, last_response.status
+    assert_equal $test_db[:graphics][random_id].title, JSON.parse(last_response.body).title
+  end
+
+  it "should get all variants" do
+    random_id = (rand 9).to_i
+    get "graphic/#{random_id}/variants"
+
+    # all variants should be available at once
+    assert_equal 200, last_response.status
+    assert_equal $test_db[:graphics][random_id].title, JSON.parse(last_response.body).title
+  end
+end
