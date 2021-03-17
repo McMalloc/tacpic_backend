041749a4dd17605aac40335a87a4470af61c8f98
Robert Wlcek <robert@tacpic.de>
2021-03-17 18:47:46 +0100

    * geändert: vollständige Backup-Routine

--- backup.rb
+++ backup.rb
@@ -1,14 +1,33 @@
 require 'net/sftp'
+require 'zip'
+require 'date'
 
 def backup
-  Net::SFTP.start('ngcobalt327.manitu.net', 'ftp200008186', password: 'HPpHZ5vXh4xUC272fWJy', port: 23) do |sftp|
-    Dir.mkdir 'backups' unless Dir.exist? 'backups'
-    `pg_dump #{ENV['TACPIC_DATABASE_URL']} -f ./backups/#{ENV['RACK_ENV']}-dump.sql`
+  timestamp = DateTime.now.strftime('%Y-%m-%d_%H-%M-%S')
+  Dir.mkdir 'backups' unless Dir.exist? 'backups'
+  `pg_dump #{ENV['TACPIC_DATABASE_URL']} -f ./backups/#{timestamp}__#{ENV['RACK_ENV']}-dump.sql`
 
+  Zip::File.open("./backups/#{timestamp}__files.zip", Zip::File::CREATE) do |zipfile|
+    Dir['./files/*'].each do |file|
+      unless File.directory? file
+        zipfile.add "#{File.basename(file)}.#{File.extname(file)}", file
+      end
+    end
+  end
+
+  Zip::File.open("./backups/#{timestamp}__thumbnails.zip", Zip::File::CREATE) do |zipfile|
+    Dir['./files/thumbnails/*'].each do |file|
+      unless File.directory? file
+        zipfile.add "#{File.basename(file)}.#{File.extname(file)}", file
+      end
+    end
+  end
+
+  Net::SFTP.start(ENV['BACKUP_HOST'], ENV['BACKUP_USER'], password: ENV['BACKUP_PWD'], port: ENV['BACKUP_PORT']) do |sftp|
     sftp.dir.foreach('/') do |entry|
       puts entry.longname
     end
 
-    sftp.upload!("./backups/#{ENV['RACK_ENV']}-dump.sql", "/#{ENV['RACK_ENV']}-dump.sql")
+    sftp.upload!("./backups/#{timestamp}__#{ENV['RACK_ENV']}-dump.sql", "/#{timestamp}__#{ENV['RACK_ENV']}-dump.sql")
   end
 end
